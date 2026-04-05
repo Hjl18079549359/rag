3b0b08a1f3f2b15cd6d29e743e7c0829b608ee2c
whoishuang00 <2994366975@qq.com>
2026-04-05 17:00:45 +0800

    历史会话记录功能的代码开发

--- rag.py
+++ rag.py
@@ -1,9 +1,9 @@
 from langchain_community.embeddings import DashScopeEmbeddings
 from langchain_core.documents import Document
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
-
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnablePassthrough, RunnableWithMessageHistory, RunnableLambda
+from file_history_store import get_history
 import config_data as config
 from vector_stores import VectorStoreService
 from langchain_community.chat_models.tongyi import ChatTongyi
@@ -22,6 +22,8 @@ class RagService(object):
         self.prompt_template=ChatPromptTemplate.from_messages(
             [
                 ("system","以我提供的已知参考资料为主，简洁和专业的回答用户问题，参考资料：{context}"),
+                ("system","并且我提供用户的对话历史记录，如下"),
+                MessagesPlaceholder("history"),
                 ("user","请回答用户提问：{input}")
             ]
         )
@@ -37,13 +39,36 @@ class RagService(object):
             for doc in docs:
                 format_str+=f"文档片段：{doc.page_content}\n文档元数据：{doc.metadata}\n\n"
             return format_str
+
+        def format_for_retriever(value)->str:
+            return value["input"]
+        def format_for_prompt_template(value):
+            new_value={}
+            new_value["input"]=value["input"]["input"]
+            new_value["context"]=value["context"]
+            new_value["history"]=value["input"]["history"]
+            return new_value
+
         chain=(
-            {"input":RunnablePassthrough(),"context":retriever|format_document}
-            |self.prompt_template|print_prompt|self.chat_model|StrOutputParser()
+            {"input":RunnablePassthrough(),
+             "context":RunnableLambda(format_for_retriever)|retriever|format_document}
+            |RunnableLambda(format_for_prompt_template)|self.prompt_template|print_prompt|self.chat_model|StrOutputParser()
         )
-        return chain
+        conversation_chain= RunnableWithMessageHistory(
+            chain,
+            get_history,
+            input_messages_key="input",
+            history_messages_key="history"
+        )
+        return conversation_chain
 if __name__ == '__main__':
+    # session_id配置
+    session_config={
+        "configurable":{
+            "session_id":"huang",
+        }
+    }
     service=RagService()
     chain=service.chain
-    res=chain.invoke("体重150，尺码推荐")
+    res=chain.invoke({"input":"身高165，尺码推荐"},session_config)
     print(res)
